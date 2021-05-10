df119cc8a07160495ae437c8f10b3b3bb9099330
nolly <63003496+steibern@users.noreply.github.com>
2021-05-10 10:57:10 +0200

    ....

--- src/main/java/com/example/helloworld/main.java
+++ src/main/java/com/example/helloworld/main.java
@@ -3,16 +3,28 @@ package com.example.helloworld;
 public class Main {
     public static void main (String[] args) {
 
+
+
         System.out.print(randomNumber());
     }
 
     public static int randomNumber(){
-        int min = 1;
-        int max = 600;
+        float min = 1;
+        float max = 600;
 
         return (int)Math.floor(Math.random()*(max-min+1)+min);
 
-        
+
     }
+
+    public static void reactions(){
+        int p = randomNumber();
+        if (p < 50 && p >= 0){
+            System.out.println("Akustisches Signal und Benachrichtigung an Betriebsführer");
+        }
+        else if (p >= 301 && p <= 501)
+
+    }
+
 
 }
