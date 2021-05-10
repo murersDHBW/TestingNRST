035f94ba5540d4aaf58a72ff0934dcb4cf6fb0e4
murermader <46532999+murermader@users.noreply.github.com>
2021-05-10 14:51:36 +0200

    Code zur Druckprüfung aktualisiert

--- src/test/java/com/example/helloworld/DruckpruefungTest.java
+++ src/test/java/com/example/helloworld/DruckpruefungTest.java
@@ -1,16 +1,40 @@
 package com.example.helloworld;
 
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DruckpruefungTest {
 
     @Test
-    void negativNumber(){
-        int testvalue = -1;
-        assertTrue(testvalue > 500);
+    @DisplayName("Statuscode prüfen")
+    @Description("Überprüft ob der korrekte StatusCode für den Status des Tanks zurückgegeben wird")
+    void CheckForCorrectStatusCode(){
 
-        //assertEquals(testvalue);
+        Tank t1 = new Tank();
+        StatusCode statusCode = Notifications.GetStatusForTankQuantity(t1.getQuantity());
+
+        if (statusCode.TankPressure <= 49 && statusCode.TankPressure >= 1){
+            assertEquals(statusCode.ID, StatusCodes.CriticalUnderpresssure.ID);
+        }
+        else if (statusCode.TankPressure >= 301 && statusCode.TankPressure <= 501){
+            assertEquals(statusCode.ID, StatusCodes.CriticalOverpressure.ID);
+        }
+        else if(statusCode.TankPressure >= 501){
+            assertEquals(statusCode.ID, StatusCodes.DangerAhead.ID);
+        }
+        else if(statusCode.TankPressure >= 181 && statusCode.TankPressure<= 219){
+            assertEquals(statusCode.ID, StatusCodes.OptimalQuantity.ID);
+        }
+        else if(statusCode.TankPressure >= 50 && statusCode.TankPressure<= 180){
+            assertEquals(statusCode.ID, StatusCodes.Underpressure.ID);
+        }
+        else if(statusCode.TankPressure >= 220){
+            assertEquals(statusCode.ID, StatusCodes.Overpressure.ID);
+        }
+        else {
+            assertEquals(statusCode.ID, StatusCodes.StatusError.ID);
+        }
     }
 }
