55cf679cb2d44d806cf5e36a1c7b7a9788d9e427
nolly <63003496+steibern@users.noreply.github.com>
2021-05-10 11:16:14 +0200

    Reactions

--- src/main/java/com/example/helloworld/main.java
+++ src/main/java/com/example/helloworld/main.java
@@ -7,7 +7,7 @@ class Main {
 
 
 
-        System.out.print(randomNumber());
+        System.out.print(reactions());
     }
 
     public static double randomNumber(){
@@ -15,6 +15,7 @@ class Main {
         double max = 600;
 
         double randomFloatNumber = min + Math.random() * (max - min);
+        randomFloatNumber = (Math.round(randomFloatNumber * 100.0)/ 100.0);
         return randomFloatNumber;
 
 //        return (int)Math.floor(Math.random()*(max-min+1)+min);
@@ -22,14 +23,29 @@ class Main {
 
     }
 
-    public static void reactions(){
+    public static String reactions(){
+
         double p = randomNumber();
-        if (p < 50 && p >= 0){
-            System.out.println("Akustisches Signal und Benachrichtigung an Betriebsführer");
+        String answer = "";
+
+
+        if (p <= 49 && p >= 1){
+            answer = "Füllstand:" + p + ".Akustisches Signal und Benachrichtigung an Betriebsführer";
         }
         else if (p >= 301 && p <= 501){
-            // do sth
+            answer = "Füllstand:" + p + ".Akustisches Signal, Benachrichtigung an Betriebsführer und Information an Wartungsteam";
         }
+        else if(p >= 501){
+            answer = "Füllstand:" + p + ".Alarm auslösen und Evakuierung des Gebäudes";
+        }
+        else if(p >= 181 && p<= 219){
+            answer = "Füllstand:" + p + ". Der Stand ist in einem Soliden gediegenen Bereich";
+        }
+        else if(p >= 50 && p<= 180 || p >= 220 && p<= 300){
+            answer = "Füllstand:" + p + ". Benachrichtigung an Betriebsführer und Information an Wartungsteam";
+        }
+
+        return answer;
 
     }
 
