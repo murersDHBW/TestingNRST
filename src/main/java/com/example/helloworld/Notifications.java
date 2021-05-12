3786c967e24ad456f2357ddefb336daccb48535d
murermader <46532999+murermader@users.noreply.github.com>
2021-05-12 10:00:33 +0200

    Methoden in Notifications nun public

--- src/main/java/com/example/helloworld/Notifications.java
+++ src/main/java/com/example/helloworld/Notifications.java
@@ -2,31 +2,31 @@ package com.example.helloworld;
 
 public class Notifications {
 
-    public void ReactToStatusCode(StatusCode code){
-        if(code == TankStatusCodes.DangerAhead){
+    public void ReactToStatusCode(StatusCode code) {
+        if (code == TankStatusCodes.DangerAhead) {
             Alarm();
-        } else if(code == TankStatusCodes.CriticalUnderpresssure){
+        } else if (code == TankStatusCodes.CriticalUnderpresssure) {
             PlayAcousticSignal();
             SendNotification(code.StatusMessage, "Betriebsführer");
-        } else if(code == TankStatusCodes.CriticalOverpressure){
+        } else if (code == TankStatusCodes.CriticalOverpressure) {
             PlayAcousticSignal();
             SendNotification(code.StatusMessage, "Betriebsführer");
             SendNotification(code.StatusMessage, "Wartungsteam");
-        } else if(code == TankStatusCodes.Underpressure || code == TankStatusCodes.Overpressure){
+        } else if (code == TankStatusCodes.Underpressure || code == TankStatusCodes.Overpressure) {
             SendNotification(code.StatusMessage, "Betriebsführer");
             SendNotification(code.StatusMessage, "Wartungsteam");
         }
     }
 
-    private void Alarm(){
+    public void Alarm() {
         System.out.println("ALARM!!!");
     }
 
-    private void PlayAcousticSignal(){
+    void PlayAcousticSignal() {
         System.out.println("Es wird ein Ton abgespielt");
     }
 
-    private void SendNotification(String msg, String recipient){
+    public void SendNotification(String msg, String recipient) {
         System.out.println("Es wird eine Nachricht mit dem Inhalt [" + msg + "] an [" + recipient + "] geschickt");
     }
 
